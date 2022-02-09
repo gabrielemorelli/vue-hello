487904d3b04ce0ae70177a99a5b6a3d1326cefbb
MyUsernme <gabriele.morelli@live.it>
2022-02-09 16:43:46 +0100

    esercizio bonus

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-const root = new Vue (
+const root = new Vue(
     {
         el: "#root",
         data: {
-            messaggio:'Ciao',
-         
-        },
+            messaggio: "ciao",
+            immagine: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi0.wp.com%2Fwww.giacomocusano.com%2Fwp-content%2Fuploads%2F2016%2F07%2Fcoastal-wash-web.jpg%3Ffit%3D1024%252C682%26ssl%3D1&imgrefurl=https%3A%2F%2Fwww.giacomocusano.com%2Fimmagini-ad-alta-risoluzione-gratis%2F&tbnid=hqwPGBF2l1kOmM&vet=12ahUKEwiq99Oy7_L1AhURahoKHShOAjQQMygBegUIARDYAQ..i&docid=oi8HLsCd2qa-OM&w=1024&h=682&q=immagini&ved=2ahUKEwiq99Oy7_L1AhURahoKHShOAjQQMygBegUIARDYAQ",
+
+        }
     }
 );
